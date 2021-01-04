38d272c5a327eb5439722d3f62349791b1ee526c
GitHub Action <action@github.com>
2021-01-04 07:35:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/wzgcoder/templatedemo/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wzgcoder/templatedemo/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wzgcoder.templatedemo.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wzgcoder.templatedemo.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wzgcoder/templatedemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wzgcoder/templatedemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wzgcoder.templatedemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wzgcoder.templatedemo.MyBundle
 
 class MyApplicationService {
 
